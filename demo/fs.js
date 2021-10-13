04189e099820313f272527e02f0f64a806b95fd8
RomanKamshilov <kursi.kamsh@gmail.com>
2021-10-13 20:34:10 +0300

    upload-project
    Upload all files

---
+++ demo/fs.js
@@ -0,0 +1,42 @@
+const fs = require('fs')
+const path = require('path')
+const {stderr} = require("chalk");
+
+// fs.mkdir(path.join(__dirname, 'test'), (err) => {
+//     if (err) {
+//         throw err
+//     }
+//
+//     console.log('Папка создана')
+// })
+
+const filePath = path.join(__dirname, 'test', 'text.txt')
+//
+// fs.writeFile(filePath, 'Hello NodeJS', err => {
+//     if (err) {
+//         throw err
+//     }
+//
+//     console.log('Файл создан')
+//
+//     fs.appendFile(filePath, '\nHello Again', err => {
+//         if (err) {
+//             throw err
+//         }
+//
+//         console.log('Файл создан')
+//     })
+// })
+
+fs.readFile(filePath, 'utf-8' ,(err, content) => {
+   if (err) {
+       throw err
+   }
+
+    console.log(content)
+
+   // const data = Buffer.from(content)
+   // console.log('Content: ', data.toString())
+
+
+})
---
+++ test.js
@@ -0,0 +1,7 @@
+
+const plus = (one, two) => (one + two)
+const perem = plus(4,6)
+
+const work = 'allright'
+
+module.exports = `${work} и ${perem}`
